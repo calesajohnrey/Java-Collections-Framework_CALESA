447d4fb83e72b9f84eae3e3c94714e1e8cb3fdea
John <21103870@usc.edu.ph>
2022-09-29 15:15:46 +0800

    updated the code

--- Hashmap.java
+++ Hashmap.java
@@ -1,14 +1,13 @@
-//This is program only accepts numbers.
+//This is program accepts strings and numbers.
 //Please be warned!
 
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Hashmap { 
-public static HashMap<String, Integer> foods;
+public class Hashmap {
+    public static HashMap<String, Integer> foods;
 
-    
-static void addElement(){
+    static void addElement() {
         String food1;
         int food2;
         boolean idtfr = false;
@@ -16,23 +15,24 @@ static void addElement(){
         System.out.println("");
         System.out.println(" --------------------------------------------- ");
         System.out.println("This is the interface for adding an element!");
-        System.out.print("Enter the item one here: ");
+        System.out.println("");
+        System.out.print("Enter the key-name here (String only): ");
         food1 = input.next();
-        System.out.print("Enter the new item here: ");
+        System.out.print("Enter the value here (Integer only): ");
         food2 = input.nextInt();
-        for (int i = 2; i <= food2/2; ++i) {
+        for (int i = 2; i <= food2 / 2; ++i) {
             if (food2 % i == 0) {
-            idtfr = true;
-            break;
+                idtfr = true;
+                break;
+            }
         }
-    }
-        if (!idtfr){
+        if (!idtfr) {
             System.out.println("");
             System.out.println(food2 + " is a prime number.");
             System.out.println("Enter another number.");
             addElement();
         } else {
-            foods.put(food1,food2);
+            foods.put(food1, food2);
             System.out.println(" ");
             System.out.println(foods);
             System.out.println(" ");
@@ -40,7 +40,7 @@ static void addElement(){
         }
     }
 
-static void removeElement(){
+    static void removeElement() {
         System.out.println("");
         System.out.println("This is the interface for removing an element!");
         System.out.println(" --------------------------------------------- ");
@@ -48,20 +48,20 @@ static void removeElement(){
         System.out.println(foods);
         System.out.println(" --------------------------------------------- ");
         Scanner prompt = new Scanner(System.in);
-        System.out.print("Enter the item here: ");
+        System.out.println("");
+        System.out.print("Enter the key-name here (String only): ");
         Object newInputItem = prompt.nextLine();
-        if (newInputItem instanceof Integer){
+        if (newInputItem instanceof Integer) {
             System.out.println("Invalid input");
         } else {
-            if ("".equals(newInputItem)){
-            System.out.println("No value entered!");
-            System.out.println("Try again");
-            removeElement(); 
-            }
-            else {
+            if ("".equals(newInputItem)) {
+                System.out.println("No value entered!");
+                System.out.println("Try again");
+                removeElement();
+            } else {
                 String newItem = newInputItem.toString();
                 String newItems = newItem.toLowerCase();
-                if (foods.containsKey(newItems)){
+                if (foods.containsKey(newItems)) {
                     foods.remove(String.valueOf(newItem));
                     System.out.println(foods);
                     prompt();
@@ -72,19 +72,19 @@ static void removeElement(){
                 }
             }
         }
-    } 
+    }
 
-static void displayElement(){
-            System.out.println("");
-            System.out.println("This is the interface for viewing all element!");
-            System.out.println("+---------------------------------------------+");
-            System.out.println("This are the elements in the HashMap: ");
-            System.out.println(foods);
-            System.out.println("");
-            prompt();
-        }
+    static void displayElement() {
+        System.out.println("");
+        System.out.println("This is the interface for viewing all element!");
+        System.out.println("+---------------------------------------------+");
+        System.out.println("This are the elements in the HashMap: ");
+        System.out.println(foods);
+        System.out.println("");
+        prompt();
+    }
 
-static void clearArray(){
+    static void clearArray() {
         Scanner prompt = new Scanner(System.in);
         System.out.println(" ");
         System.out.println("+---------------------------------------------+");
@@ -97,60 +97,62 @@ static void clearArray(){
         if (null == answer) {
             System.out.println("Invalid input.");
             clearArray();
-        } else switch (answer) {
-            case "y":
-                System.out.println(" ");
-                System.out.println("Exiting the program!");
-                foods.clear();
-                System.out.println(foods);
-                intro();
-            
-            case "n":
-                prompt();
-            default:
-                System.out.println("Invalid input.");
-                clearArray();
+        } else
+            switch (answer) {
+                case "y":
+                    System.out.println(" ");
+                    System.out.println("Exiting the program!");
+                    foods.clear();
+                    System.out.println(foods);
+                    intro();
+
+                case "n":
+                    prompt();
+                default:
+                    System.out.println("Invalid input.");
+                    clearArray();
+            }
+    }
+
+    static void intro() {
+        System.out.println(" ");
+        System.out.println(" --------------------------------------------- ");
+        System.out.println("This are the elements in the HashMap: ");
+        System.out.println(foods);
+        System.out.println(" --------------------------------------------- ");
+        Scanner input = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("Please select your option:");
+        System.out.println("A. Add an element in the HashMap.");
+        System.out.println("B. Remove an element in the HashMap.");
+        System.out.println("C. Display all elements in the HashMap.");
+        System.out.println("D. Clear elements in the HashMap.");
+        System.out.println("E. Exit the program!");
+        System.out.println(" ");
+        System.out.print("Enter it here: ");
+        String inputChar = input.nextLine();
+        String convertChar = inputChar.toUpperCase();
+        if (convertChar instanceof String) {
+            switch (convertChar) {
+                case "A":
+                    addElement();
+                case "B":
+                    removeElement();
+                case "C":
+                    displayElement();
+                case "D":
+                    clearArray();
+                case "E":
+                    exitElement();
+                default:
+                    System.out.println(" ");
+                    System.out.println("Invalid input");
+                    intro();
+            }
         }
     }
 
-static void intro(){
-    System.out.println(" ");
-    System.out.println(" --------------------------------------------- ");
-    System.out.println("This are the elements in the HashMap: ");
-    System.out.println(foods);
-    System.out.println(" --------------------------------------------- ");
-    Scanner input = new Scanner(System.in);
-    System.out.println(" ");
-    System.out.println("Please select your option:");
-    System.out.println("A. Add an element in the HashMap.");
-    System.out.println("B. Remove an element in the HashMap.");
-    System.out.println("C. Display all elements in the HashMap.");
-    System.out.println("D. Clear elements in the HashMap.");
-    System.out.println("E. Exit the program!");
-    System.out.println(" ");
-    System.out.print("Enter it here: ");
-    String inputChar = input.nextLine();
-    String convertChar = inputChar.toUpperCase();
-    if (convertChar instanceof String) {
-        switch (convertChar) {
-            case "A":
-                addElement(); 
-            case "B":
-                removeElement();
-            case "C":
-                displayElement();
-            case "D":
-                clearArray();
-            case "E":
-                exitElement();
-            default:
-                System.out.println("Invalid input");
-                intro();
-        }
-    }
-}
-
-static void prompt(){
+    static void prompt() {
         System.out.println(" ");
         System.out.println("-----------------------------------------------");
         Scanner prompt = new Scanner(System.in);
@@ -159,23 +161,21 @@ static void prompt(){
         System.out.print("Enter it here: ");
         String inputChar = prompt.nextLine();
         String answer = inputChar.toLowerCase();
-        if ("y".equals(answer)){
+        if ("y".equals(answer)) {
             System.out.println(" ");
-                intro();
-        }
-        else if ("n".equals(answer)){
+            intro();
+        } else if ("n".equals(answer)) {
             System.out.println(" ");
             exitElement();
-        }
-        else {
+        } else {
             System.out.println("Invalid input.");
             System.out.println(" ");
             prompt();
         }
-        
-    } 
-    
-static void exitElement(){
+
+    }
+
+    static void exitElement() {
         Scanner prompt = new Scanner(System.in);
         System.out.println(" ");
         System.out.println("-----------------------------------------------");
@@ -189,26 +189,26 @@ static void exitElement(){
             System.out.println("Invalid input.");
             System.out.println(" ");
             exitElement();
-        } else switch (answer) {
-            case "y":
-                System.out.println("Exiting the program!");
-                System.exit(0);
-            case "n":
-                System.out.println(" ");
-                prompt();
-                break;
-            default:
-                System.out.println(" ");
-                System.out.println("Invalid input.");
-                System.out.println(" ");
-                exitElement();
-                break;
-        } 
+        } else
+            switch (answer) {
+                case "y":
+                    System.out.println("Exiting the program!");
+                    System.exit(0);
+                case "n":
+                    System.out.println(" ");
+                    prompt();
+                    break;
+                default:
+                    System.out.println(" ");
+                    System.out.println("Invalid input.");
+                    System.out.println(" ");
+                    exitElement();
+                    break;
+            }
     }
 
-public static void main(String[] args) {
-    foods = new HashMap<String, Integer>();
-    intro();
-  }
+    public static void main(String[] args) {
+        foods = new HashMap<String, Integer>();
+        intro();
+    }
 }
-
