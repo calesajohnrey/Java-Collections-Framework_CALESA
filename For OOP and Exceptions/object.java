f8459218cc2ee646e9228259a255ef957a7108f5
John <21103870@usc.edu.ph>
2022-10-04 21:45:14 +0800

    update the code in object.java

--- For OOP and Exceptions/object.java
+++ For OOP and Exceptions/object.java
@@ -1,3 +1,11 @@
-public class object<String>{
-    object name;
+public class object <String>{
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String newName) {
+    this.name = newName;
+  }
 }
