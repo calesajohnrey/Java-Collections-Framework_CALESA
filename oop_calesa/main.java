693d9155834cea0e1ab8d3c05921bd67f661fd05
John <21103870@usc.edu.ph>
2022-10-07 14:31:08 +0800

    update the main code

--- oop_calesa/main.java
+++ oop_calesa/main.java
@@ -1,14 +1,27 @@
-
 package oop_calesa;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
+    public static ArrayList<Integer> numbers;
     public static void main(String[] args) {
-        object<Integer> newestNameAsInteger;
-        newestNameAsInteger = new object<String>(15);
-        System.out.println(newestNameAsInteger.getName());
-  
-        object<String> newestNameAsString;
-        newestNameAsString = new object<String>("This is me, John!");
-        System.out.println(newestNameAsString.getName());
+        try {
+            System.out.println("Please enter the size of the array: ");
+            Scanner number = new Scanner(System.in); 
+            String numberArrStr = number.next();
+            Integer nums = Integer.parseInt(numberArrStr);
+            numbers = new ArrayList<>(nums);
+            Scanner prompt = new Scanner(System.in);
+            System.out.print("Enter the new item here: ");
+            String newInputItem = prompt.next(); 
+            Integer newObject = Integer.parseInt(newInputItem);
+            numbers.add(newObject);
+            System.out.println(" ");
+            System.out.println("You set the number of items in the Array!");
+            System.out.println(" ");
+            System.out.println(numbers);  
+        } catch (Exception e) {
+            System.out.print("Error!");
+        }
     }
 }
